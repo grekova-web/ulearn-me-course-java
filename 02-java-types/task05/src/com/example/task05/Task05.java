784b479aa5885fafadfc909e040ab090de382b5b
grekova-web <71182152+grekova-web@users.noreply.github.com>
2020-09-22 18:59:31 +0500

    Грекова Елена. Задание 2.5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -4,9 +4,13 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        while (x > 0) {
+            int a = x % 10;
+            x /= 10;
+            if ((a % 2) != 0)
+                return "FALSE";
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
